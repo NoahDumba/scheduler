a09a233733a2a0e4019592058d3fd99ac367962e
NoahDumba <noahdumba1@gmail.com>
2020-02-17 15:41:55 -0700

    implemented the interviewer item and interviewer list

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+
+const classNames = require('classnames');
+
+export default function InterviewerListItem(props) {
+  const interviewersClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected === true
+  });
+
+  return (
+    <li className={interviewersClass} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
