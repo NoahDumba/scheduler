b9ae0affc2f2246f7e2c9d12ce666ac5617854fd
NoahDumba <noahdumba1@gmail.com>
2020-02-18 09:40:04 -0700

    fixed bug

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,47 @@
 import React, { useState } from "react";
+import Appointment from "components/Appointment";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Noah Dumbass",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
 
 const days = [
   {
@@ -47,7 +87,14 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+      {
+        appointments.map(appointment => {
+          return (
+            <Appointment key={appointment.id} {...appointment} />
+          );
+        })
+      }
+      { <Appointment key="last" time="5pm" /> }
       </section>
     </main>
   );
